d93ffd3794d0a59961ffc28080f8cf6d44d03b51
meruyert.bekzhigit <meruyert.bekzhigit@gmail.com>
2022-07-14 18:25:31 +0200

    Get real time data

--- lib/models/launch.dart
+++ lib/models/launch.dart
@@ -3,14 +3,12 @@ import 'dart:ui';
 class Launch {
   String name;
   String date;
-  String icon;
   String payloadNumber;
 
   Launch(
     {
     this.name = "",
     this.date = "",
-    this.icon = "",
     this.payloadNumber = ""
     }
   );
--- lib/pages/launchlistpage.dart
+++ lib/pages/launchlistpage.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:sample_listing_app/utils.dart';
+import 'package:sample_listing_app/helpers/utils.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/launch.dart';
 
@@ -7,6 +10,19 @@ class LaunchListPage extends StatelessWidget {
   static const IconData star = IconData(0xe5f9, fontFamily: 'MaterialIcons');
   List<Launch> launches = Utils.getMockedLaunches();
 
+  Future getLaunchData() async {
+    var response = await http.get(Uri.https('api.spacexdata.com', 'v4/launches'));
+    var jsonData = jsonDecode(response.body);
+    List<Launch> fetchedLaunches = [];
+
+    for (var data in jsonData){
+      Launch launch = Launch(name: data['name'], date: data['date_utc'], payloadNumber: data['payloads']);
+      fetchedLaunches.add(launch);
+    }
+    print(fetchedLaunches.length);
+    return fetchedLaunches;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +34,13 @@ class LaunchListPage extends StatelessWidget {
           itemBuilder: (BuildContext ctx, int index) {
            return Container(
              margin: EdgeInsets.all(20),
-             height: 150,
+             height: 100,
              child: ListTile(
                       leading: Icon(Icons.map),
                       title: Text(launches[index].name),
                       subtitle: Text(launches[index].date),
                       trailing: Icon(star)
                   ),
-
-
            );
           }
         ),
