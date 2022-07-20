27dc3dfe2e6636ba9522c72b1fb81e5281b753be
meruyert.bekzhigit <meruyert.bekzhigit@gmail.com>
2022-07-20 23:56:41 +0200

    Simplified to a list with name property only

--- lib/pages/launchlistpage.dart
+++ lib/pages/launchlistpage.dart
@@ -16,23 +16,6 @@ class LaunchListPage extends StatefulWidget {
 class LaunchListPageState extends State<LaunchListPage> {
   static const IconData star = IconData(0xe5f9, fontFamily: 'MaterialIcons');
   List<Launch> launches = Utils.getMockedLaunches();
-  var isSelectedAsFavourite = false;
-  int _selectedIndex = 0;
-  bool _customTileExpanded = false;
-
-  bool isExpanded = false;
-
-  Future getLaunchData() async {
-    var response = await http.get(Uri.https('api.spacexdata.com', 'v4/launches'));
-    var jsonData = jsonDecode(response.body);
-    List<Launch> fetchedLaunches = [];
-
-    for (var data in jsonData){
-      Launch launch = Launch(name: data['name'], date: data['date_utc'], payloadNumber: data['payloads']);
-      fetchedLaunches.add(launch);
-    }
-    return fetchedLaunches;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +35,6 @@ class LaunchListPageState extends State<LaunchListPage> {
              child: Column(
                children: [
                  ListTile(
-                     selected: index == _selectedIndex,
                      leading: const Icon(
                        Icons.auto_graph,
                        size: 50,
@@ -64,39 +46,8 @@ class LaunchListPageState extends State<LaunchListPage> {
                            color: Colors.black
                        ),
                      ),
-                     subtitle: Column(
-                       crossAxisAlignment: CrossAxisAlignment.start,
-                       children: <Widget>[
-                         const SizedBox(height: 10),
-                         Text(
-                           launches[index].date,
-                           style: const TextStyle(
-                               fontWeight: FontWeight.normal,
-                               color: Colors.black
-                           ),
-                         ),
-                       ],
-                     ),
-                     trailing: IconButton(
-                       icon: const Icon(Icons.stars_sharp),
-                       tooltip: 'Search',
-                       iconSize: 32.0,
-                       color: (index == _selectedIndex && isSelectedAsFavourite) ? Colors.amber : Colors.grey,
-                       onPressed: () {
-                         setState(() //<--whenever icon is pressed, force redraw the widget
-                         {
-                           _selectedIndex = index;
-                           isSelectedAsFavourite  = !isSelectedAsFavourite;
-                         }
-                         );
-                       },
-                     ),
                      onTap: () { /* react to the tile being tapped */ print("CELL IS TAPPED"); }
                  ),
-                   Container(
-                     color: Colors.white,
-                     child: buildExpansionPanel(context),
-                   ),
                ],
              )
            );
@@ -104,28 +55,5 @@ class LaunchListPageState extends State<LaunchListPage> {
         ),
       );
   }
-
-  @override
-  Widget buildExpansionPanel(BuildContext context) => const ExpansionTile(
-    title: Text('Payload count: 2'),
-    children: [
-      Text(
-        'RazakSAT',
-        style: TextStyle(
-          fontSize: 16,
-          height: 1.4,
-        ),
-        textAlign: TextAlign.left,
-      ),
-      Text(
-        'PreSAT',
-        style: TextStyle(
-          fontSize: 16,
-          height: 1.4,
-        ),
-        textAlign: TextAlign.left,
-      )
-    ],
-  );
 }
 
