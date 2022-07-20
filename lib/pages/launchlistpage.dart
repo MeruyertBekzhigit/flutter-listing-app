7a3eb6b876d0173cef733c49225d90a10adbbb4c
meruyert.bekzhigit <meruyert.bekzhigit@gmail.com>
2022-07-20 13:10:43 +0200

    Moved out payloads to expansion panel

--- lib/pages/launchlistpage.dart
+++ lib/pages/launchlistpage.dart
@@ -18,6 +18,9 @@ class LaunchListPageState extends State<LaunchListPage> {
   List<Launch> launches = Utils.getMockedLaunches();
   var isSelectedAsFavourite = false;
   int _selectedIndex = 0;
+  bool _customTileExpanded = false;
+
+  bool isExpanded = false;
 
   Future getLaunchData() async {
     var response = await http.get(Uri.https('api.spacexdata.com', 'v4/launches'));
@@ -46,71 +49,68 @@ class LaunchListPageState extends State<LaunchListPage> {
              color: const Color(0xffbbbcbd),
              margin: const EdgeInsets.only(left: 10, right: 10, top: 5),
              padding: const EdgeInsets.only(left: 5, top: 20, right: 5, bottom: 10),
-             child: ListTile(
-                 selected: index == _selectedIndex,
-                 leading: const Icon(
-                   Icons.auto_graph,
-                   size: 50,
-                 ),
-                 title: Text(
-                   launches[index].name,
-                   style: const TextStyle(
-                       fontWeight: FontWeight.bold,
-                       color: Colors.black
-                   ),
-                 ),
-                 subtitle: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                   children: <Widget>[
-                     const SizedBox(height: 10),
-                     Text(
-                       launches[index].date,
+             child: Column(
+               children: [
+                 ListTile(
+                     selected: index == _selectedIndex,
+                     leading: const Icon(
+                       Icons.auto_graph,
+                       size: 50,
+                     ),
+                     title: Text(
+                       launches[index].name,
                        style: const TextStyle(
-                           fontWeight: FontWeight.normal,
+                           fontWeight: FontWeight.bold,
                            color: Colors.black
                        ),
                      ),
-                     Row(
+                     subtitle: Column(
+                       crossAxisAlignment: CrossAxisAlignment.start,
                        children: <Widget>[
-                         const Text(
-                           "Payload count: 12",
-                           style: TextStyle(
+                         const SizedBox(height: 10),
+                         Text(
+                           launches[index].date,
+                           style: const TextStyle(
                                fontWeight: FontWeight.normal,
                                color: Colors.black
                            ),
                          ),
-                         const SizedBox(width: 70),
-                         IconButton(
-                           onPressed: () {},
-                           icon: const Icon(
-                               Icons.arrow_downward,
-                               size: 30,
-                               color: Colors.black
-                           ),
-                         )
                        ],
-                     )
-                   ],
+                     ),
+                     trailing: IconButton(
+                       icon: const Icon(Icons.stars_sharp),
+                       tooltip: 'Search',
+                       iconSize: 32.0,
+                       color: (index == _selectedIndex && isSelectedAsFavourite) ? Colors.amber : Colors.grey,
+                       onPressed: () {
+                         setState(() //<--whenever icon is pressed, force redraw the widget
+                         {
+                           _selectedIndex = index;
+                           isSelectedAsFavourite  = !isSelectedAsFavourite;
+                         }
+                         );
+                       },
+                     ),
+                     onTap: () { /* react to the tile being tapped */ print("CELL IS TAPPED"); }
                  ),
-                 trailing: IconButton(
-                   icon: const Icon(Icons.stars_sharp),
-                   tooltip: 'Search',
-                   iconSize: 32.0,
-                   color: (index == _selectedIndex && isSelectedAsFavourite) ? Colors.amber : Colors.grey,
-                   onPressed: () {
-                     setState(() //<--whenever icon is pressed, force redraw the widget
-                     {
-                       _selectedIndex = index;
-                       isSelectedAsFavourite  = !isSelectedAsFavourite;
-                     }
-                     );
-                   },
-                 ),
-                 onTap: () { /* react to the tile being tapped */ print("CELL IS TAPPED"); }
-             ),
+                 buildExpansionPanel(context),
+               ],
+             )
            );
           }
         ),
       );
   }
+
+  @override
+  Widget buildExpansionPanel(BuildContext context) => const ExpansionTile(
+    title: Text('Payload count: 12'),
+    children: [
+      Text(
+        'RazakSAT',
+        style: TextStyle(fontSize: 18, height: 1.4),
+      )
+    ],
+  );
 }
+
