a0ac337ed8b94b70262ba14436d93d7d94250476
meruyert.bekzhigit <meruyert.bekzhigit@gmail.com>
2022-07-22 13:30:19 +0200

    Mock data class signature changes

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -3,10 +3,10 @@ import '../models/launch.dart';
 class Utils {
   static List<Launch> getMockedLaunches() {
     return [
-      Launch(name: "Trailblazer", date: "2021.01.21 - 19:30"),
-      Launch(name: "CRS-21", date: "2021.02.15 - 5:30"),
-      Launch(name: "Stardust-5", date: "2021.02.15 - 5:30"),
-      Launch(name: "Stardust-6", date: "2021.04.12 - 21:00"),
+      Launch(name: "Trailblazer", date: "2021.01.21 - 19:30", payloads: []),
+      Launch(name: "CRS-21", date: "2021.02.15 - 5:30", payloads: []),
+      Launch(name: "Stardust-5", date: "2021.02.15 - 5:30", payloads: []),
+      Launch(name: "Stardust-6", date: "2021.04.12 - 21:00", payloads: []),
     ];
   }
 }
